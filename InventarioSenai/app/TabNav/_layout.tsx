457fdee8bd73f7cf6c2ef3224c264dd66aed5d64
julia-015 <136704485+julia-015@users.noreply.github.com>
2024-04-24 17:30:54 -0300

    update

--- InventarioSenai/app/TabNav/_layout.tsx
+++ InventarioSenai/app/TabNav/_layout.tsx
@@ -1,7 +1,11 @@
 import { Tabs } from "expo-router";
 import {MaterialIcons} from '@expo/vector-icons';
+import { useColor } from "../../temas/Temas";  //implementação das cores novas
+import { Ionicons } from '@expo/vector-icons';
 
 export default function layout(){
+    const cores = useColor()
+    
     return(
         <Tabs screenOptions={{
             tabBarShowLabel: false,
@@ -9,19 +13,23 @@ export default function layout(){
                 <MaterialIcons name="camera-alt" size={size} color={color} />
             ),
         }}>
-            <Tabs.Screen name="Scanner" options={{
-                headerTintColor: '#595959',
-            }}/>
+            
             <Tabs.Screen name="DrawerNav" options={{
                 headerShown: false,
                 tabBarStyle:{
                     backgroundColor: '#FF0000'
                 },
+            
                 tabBarIcon: ({color, size}) => (
                     <MaterialIcons name="format-list-bulleted" size={size} color={color} />
                 ),
+            
                 tabBarInactiveTintColor: '#fff',
                 }}/>
+            
+            <Tabs.Screen name="Scanner" options={{
+                headerTintColor: '#595959',
+            }}/>
         </Tabs>
     )
 }
