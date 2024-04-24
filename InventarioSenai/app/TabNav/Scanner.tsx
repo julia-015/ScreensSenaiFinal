ac180150dbf852574d87cc4d21ac2201fda95836
julia-015 <136704485+julia-015@users.noreply.github.com>
2024-04-24 17:27:44 -0300

    update

--- InventarioSenai/app/TabNav/Scanner.tsx
+++ InventarioSenai/app/TabNav/Scanner.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, StyleSheet, ScrollView, Image } from "react-native";
 import { useColor } from "../../temas/Temas";
+import { MaterialIcons } from '@expo/vector-icons';    // para colocar o icone do scanner
 
 function Scanner() {
     
@@ -8,8 +9,8 @@ function Scanner() {
     return (
         <ScrollView style={styles.scrollView}>
             <View style={styles.container}>
-                <Image style={{ width: 350, height: 350 }} source={require('@assets/Scanner.png')} />
-            </View>
+                <MaterialIcons name="qr-code-scanner" size={350} color={cores.scannerColor} />
+            </View> 
         </ScrollView>     
     );
 }
