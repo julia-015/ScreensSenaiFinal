b013c7eaad140c9ac0697b804bced49c97fd8aa4
julia-015 <136704485+julia-015@users.noreply.github.com>
2024-04-24 17:56:15 -0300

    update

--- InventarioSenai/app/TelasIniciais/Cadastro.tsx
+++ InventarioSenai/app/TelasIniciais/Cadastro.tsx
@@ -2,14 +2,19 @@ import { View, StyleSheet, ScrollView } from 'react-native';
 import Header from '@components/Header';
 import Input from '@components/Input';
 import Botao from '@components/Botao';
+import LinkBtn from '@components/LinkBtn';
 import { Link } from 'expo-router';
-
+import { useColor } from '@temas/Temas';  // implementação de novas cores
+ 
 
 
 function Cadastro(){
+    
+    const cores = useColor()
+    
     return(
         <ScrollView>
-            <Header titulo="Cadastre-se" cor="#FF0000"/>
+            <Header titulo="Cadastre-se" cor={cores.cadastroHeader}/>
             <View style={styles.form}>
                 <Input label="Nome" placeholder={"Insira seu nome:"} />
                 <Input label="Sobrenome" placeholder={"Insira seu sobrenome:"} />
@@ -17,9 +22,7 @@ function Cadastro(){
                 <Input label="Telefone" placeholder={"Insira seu telefone:"}/>
                 <Input label="Senha" placeholder={"Insira sua senha:"} secureTextEntry={true} />
                 <Input label="Confirmar Senha" placeholder={"Insira sua senha:"} secureTextEntry={true} />
-                <Link href="TelasIniciais/Login" asChild>
-                    <Botao texto="Cadastre-se"/> 
-                </Link>
+                <LinkBtn title="Cadastre-se" href="/TelasIniciais/Login"/>
                            
             </View>
         </ScrollView>
@@ -34,6 +37,10 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         paddingBottom: 45,
         width: "100%"
+    },
+    container: {
+        backgroundColor: cores.bgPrimary,
+        flex: 1
     },
 })
 export default Cadastro
