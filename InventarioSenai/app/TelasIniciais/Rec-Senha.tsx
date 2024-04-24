f0019d2d076a41c65d430633c60f5c949202185e
julia-015 <136704485+julia-015@users.noreply.github.com>
2024-04-24 18:11:50 -0300

    update

--- InventarioSenai/app/TelasIniciais/Rec-Senha.tsx
+++ InventarioSenai/app/TelasIniciais/Rec-Senha.tsx
@@ -3,25 +3,26 @@ import Header from '@components/Header';
 import Input from '@components/Input';
 import Botao from '@components/Botao';
 import Subtitulo from '@components/Subtitulo';
+import LinkBtn from '@components/LinkBtn';
 import { Link } from 'expo-router';
+import { useColor } from '@temas/Temas';  // implementação de cores
 
 
 function RecSenha(){
     return(
         <View>
-            <Header titulo="Recuperação de senha" cor="#011E83"/>
+            <Header titulo="Recuperação de senha" cor={cores.recSenhaHeader}/>
             <View style={styles.form}>
                 <Subtitulo subtitulo="Insira seu e-mail para recuperar a senha!"/>
                 <Input label="Email" placeholder={"Insira seu email:"}/>
-                <Link href="TelasIniciais/Cadastro" asChild>
-                    <Botao texto="Cadastre-se"/> 
-                </Link>
+                <LinkBtn title="Cadastre-se" href="/TelasIniciais/Cadastro"/>
                 
             </View>
         </View>
     )
 }
 
+const cores = useColor()
 const styles = StyleSheet.create({
     form:{
         backgroundColor: "#fff",
@@ -29,6 +30,10 @@ const styles = StyleSheet.create({
         paddingTop: 45,
         paddingHorizontal: 20,
         width: "100%",
+    },
+    container: {
+        backgroundColor: cores.bgPrimary,
+        flex: 1
     },
 })
 export default RecSenha
