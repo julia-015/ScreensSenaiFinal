ac8f7f3efa4c5e3fa88c486eb75d3166df541df8
julia-015 <136704485+julia-015@users.noreply.github.com>
2024-04-24 17:36:35 -0300

    update

--- InventarioSenai/app/TabNav/DrawerNav/Home/_layout.tsx
+++ InventarioSenai/app/TabNav/DrawerNav/Home/_layout.tsx
@@ -1,8 +1,13 @@
 import { Stack } from "expo-router"
 import { DrawerToggleButton } from "@react-navigation/drawer"
+import { useColor } from "../../../../temas/Temas";    // implementação das cores
+import { Ionicons } from '@expo/vector-icons';
 
 
 export default function layout(){
+
+    const cores = useColor();
+    
     return(
         <Stack>
             <Stack.Screen name="Inventarios" options={{
@@ -14,12 +19,45 @@ export default function layout(){
                 },
                 headerTitleAlign: 'center',
                 headerTitle: 'Lista',
-                headerTintColor: '#595959'
+                headerTintColor: cores.headerTintColor,
+                headerStyle: { backgroundColor: cores.bgPrimary }
             
             }}
             
             />
+
+            <Stack.Screen 
+                name='Editar' 
+                options={{
+                    headerRight: () => <Ionicons name="checkmark-circle" size={24} color="white" />,
+                    headerTitle: "Editar",
+                    headerTintColor: "#fff",
+                    headerStyle: { backgroundColor: cores.bgSecundary },
+                    headerTitleAlign: 'center'
+                }}
+            />
+
+            <Stack.Screen 
+                name="Cad-Itens" 
+                options={{
+                    headerRight: () => <Ionicons name="add-circle-outline" size={24} color='white' />,
+                    headerTitle: "Cadastro de itens",
+                    headerTintColor: "#fff",
+                    headerStyle: { backgroundColor: cores.bgSecundary },
+                    headerTitleAlign: 'center'
+                }}
+            />
+            <Stack.Screen 
+                name="Descricao" 
+                options={{
+                    headerRight: () => <Ionicons name="refresh" size={24} color={'white'} />,
+                    headerTitle: "Descrição do item",
+                    headerTintColor: "#fff",
+                    headerStyle: { backgroundColor: cores.bgSecundary },
+                    headerTitleAlign: 'center'
+                }}
+            />
             
         </Stack>
-    )
+    );
 }
