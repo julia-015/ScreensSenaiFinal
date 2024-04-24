785171576a616a58e7c8b1b3ebf5334fcc76ad68
julia-015 <136704485+julia-015@users.noreply.github.com>
2024-04-24 17:43:44 -0300

    update.tsx

---
+++ InventarioSenai/app/TabNav/DrawerNav/Home/Cad-Itens.tsx
@@ -0,0 +1,36 @@
+import { View, StyleSheet, ScrollView } from "react-native"
+import InputItem from "@components/Input-Item"
+import { useColor } from "@temas/Temas"  // implementação de cores
+
+function CadItens(){
+    return( 
+      
+        <ScrollView style={styles.scrollview}>
+          
+                <View style={styles.container}>
+                    <InputItem label="N° do Inventário" descInicial="" />
+                    <InputItem label="Descrição " descInicial=""/>
+                    <InputItem label="Local " descInicial="" />
+                    <InputItem label="Responsável " descInicial="" />
+                    <InputItem label="Data de registro " descInicial="" />
+                </View>
+          
+        </ScrollView>
+    )
+}
+
+const cores = useColor()
+const styles = StyleSheet.create({
+
+    container:{
+        paddingVertical: 60,
+        paddingHorizontal: 20,
+        height: '100%',
+    },
+  
+    scrollview:{
+        backgroundColor: cores.bgPrimary
+    },
+  
+})
+export default CadItens
