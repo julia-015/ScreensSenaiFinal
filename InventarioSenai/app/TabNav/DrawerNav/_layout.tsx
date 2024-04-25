fbcf6b20202776122d99fb4771184e542c48250e
julia-015 <jumd.vecchia@gmail.com>
2024-04-25 15:22:17 -0300

    update - correção de erros e da camera do scanner

--- InventarioSenai/app/TabNav/DrawerNav/_layout.tsx
+++ InventarioSenai/app/TabNav/DrawerNav/_layout.tsx
@@ -1,19 +1,52 @@
 import CustomDrawer from '@components/CustomDrawer'
 import {Drawer} from 'expo-router/drawer'
-import { View } from 'react-native'
 import {MaterialIcons} from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { useColor } from '@temas/Temas';
 
 
 export default function Layout() {
+    const cores = useColor()
     return (
-        <Drawer drawerContent={(...props) => (
+        <Drawer screenOptions={{
+            drawerInactiveTintColor: cores.textColorPrimary
+        }} drawerContent={(...props) => (
             CustomDrawer(...props)
         )}>
-            <Drawer.Screen name='Home'
+
+            <Drawer.Screen name='Perfil'
                 options={{
                     headerShown: false,
-                    drawerIcon: ({size, color}) => (
-                        <MaterialIcons name='home' size={size} color={color} />
+                    drawerIcon: () => (
+                        <MaterialCommunityIcons name="account" size={24} color='#D9D9D9' />
+                    ) 
+                }}
+            />
+
+            <Drawer.Screen name='Sobre o App'
+                options={{
+                    headerShown: false,
+                    drawerIcon: () => (
+                        <MaterialCommunityIcons name="information" size={24} color="#D9D9D9" />
+                    ) 
+                }}
+            />
+
+            <Drawer.Screen name='Suporte'
+                options={{
+                    headerShown: false,
+                    drawerIcon: () => (
+                        <Ionicons name="chatbox-ellipses" size={24} color="#D9D9D9" />
+                    ) 
+                }}
+            />
+
+            <Drawer.Screen name='Sair'
+                options={{
+                    headerShown: false,
+                    drawerIcon: () => (
+                        <Ionicons name="exit" size={24} color="#D9D9D9" />
                     ) 
                 }}
             />
--- InventarioSenai/app/TabNav/Scanner.tsx
+++ InventarioSenai/app/TabNav/Scanner.tsx
@@ -1,29 +1,35 @@
-import React, { useState } from "react";
-import { View, StyleSheet, TouchableOpacity} from "react-native";
-import {MaterialIcons} from '@expo/vector-icons';
-import { Camera, CameraType } from "expo-camera";
-import { useColor } from "@temas/Temas";
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-
-const Scanner = () => {
+export default function App() {
     const [type, setType] = useState(CameraType.back);
     const [permission, requestPermission] = Camera.useCameraPermissions();
-    const [iconOpacity, setIconOpacity] = useState(1); 
-    const cores = useColor()
 
-    if (!permission)
-        return null;
+    if (!permission) {
+        return <View />;
+    }
 
-    if (!permission.granted)
-        return null;
+    if (!permission.granted) {
 
+        return (
+            <View style={styles.container}>
+                <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
 
-    return(
+    function toggleCameraType() {
+        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    }
+
+    return (
         <View style={styles.container}>
             <Camera style={styles.camera} type={type}>
-                <View style={styles.qrContainer}>
-                    <TouchableOpacity>
-                        <MaterialIcons name="qr-code-scanner" size={300} color={cores.scannerColor} style={{ opacity: iconOpacity }} />
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+                        <Text style={styles.text}>Flip Camera</Text>
                     </TouchableOpacity>
                 </View>
             </Camera>
@@ -35,19 +41,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignContent: 'center',
     },
     camera: {
         flex: 1,
-        justifyContent: 'center',
-        alignContent: 'center',
-        alignItems: 'center',
-        paddingTop: 240,
     },
-    qrContainer: {
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
         alignItems: 'center',
-        opacity: 0.2
-    }
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
 });
-
-export default Scanner;
