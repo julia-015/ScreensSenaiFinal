cf7dd7dd4def3c950804bf39b060e72dd040d9e5
julia-015 <jumd.vecchia@gmail.com>
2024-04-25 13:03:58 -0300

    update - correção de erros de sintaxe

--- InventarioSenai/app/_layout.tsx
+++ InventarioSenai/app/_layout.tsx
@@ -2,59 +2,62 @@ import { Stack } from "expo-router";
 import { Ionicons } from '@expo/vector-icons';
 import { useColor } from "../temas/Temas";
 
-export default function layout(){
+export default function layout() {
 
-  const cores = useColor()
-  
-  return(
-    <Stack>
-      <Stack.Screen name='index' options={{
-        headerShown: false
-      }}/> 
-      <Stack.Screen name='TelasIniciais/Rec-Senha' options={{
-        headerTitle: "",
-        headerTransparent: true,
-        headerTintColor: "#fff"
-      }}/> 
+  const cores = useColor();
 
-      <Stack.Screen name='TelasIniciais/Cadastro' options={{
-        headerTitle: "",
-        headerTransparent: true,
-        headerTintColor: "#fff"
-      }}/>
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name='index' options={{
+          headerShown: false
+        }}/> 
+        <Stack.Screen name='TelasIniciais/Rec-Senha' options={{
+          headerTitle: "",
+          headerTransparent: true,
+          headerTintColor: "#fff"
+        }}/> 
 
-      <Stack.Screen name="TelasIniciais/Cad-Itens" options={{
-        headerTitle: "Cadastro de itens",
-        headerTintColor: "#fff",
-        headerStyle: { backgroundColor: "#FF0000" }
-        
-      }}/>
+        <Stack.Screen name='TelasIniciais/Cadastro' options={{
+          headerTitle: "",
+          headerTransparent: true,
+          headerTintColor: "#fff"
+        }}/>
 
-      <Stack.Screen name="TelasIniciais/Descricao" options={{
-        
-        headerRight: () => (
-          <Ionicons name="refresh" size={24} color='white' />
-        ),
-        headerTitle: "Descrição do item",
-        headerTintColor: "#fff",
-        headerStyle: { backgroundColor: cores.bgSecundary },
-        headerTitleAlign: 'center'
-      }}/>
+        <Stack.Screen name="TelasIniciais/Cad-Itens" options={{
+          headerTitle: "Cadastro de itens",
+          headerTintColor: "#fff",
+          headerStyle: { backgroundColor: "#FF0000" }
+          
+        }}/>
 
-      <Stack.Screen name='TabNav' options={{
-        headerShown: false
-      }}/> 
-    </Stack>
+        <Stack.Screen name="TelasIniciais/Descricao" options={{
+          
+          headerRight: () => (
+            <Ionicons name="refresh" size={24} color='white' />
+          ),
+          headerTitle: "Descrição do item",
+          headerTintColor: "#fff",
+          headerStyle: { backgroundColor: cores.bgSecundary },
+          headerTitleAlign: 'center'
+        }}/>
 
-          <Stack.Screen name="TabNav/DrawerNav/Home/Descricao" options={{
-        
-        headerRight: () => (
-          <Ionicons name="add-circle-outline" size={24} color='white' />
-        ),
-        headerTitle: "Cadastro de itens",
-        headerTintColor: "#fff",
-        headerStyle: { backgroundColor: cores.bgSecundary },
-        headerTitleAlign: 'center'
-      }}/>
+        <Stack.Screen name='TabNav' options={{
+          headerShown: false
+        }}/> 
+      </Stack>
+
+      <div>
+        <Stack.Screen name="TabNav/DrawerNav/Home/Descricao" options={{
+          headerRight: () => (
+            <Ionicons name="add-circle-outline" size={24} color='white' />
+          ),
+          headerTitle: "Cadastro de itens",
+          headerTintColor: "#fff",
+          headerStyle: { backgroundColor: cores.bgSecundary },
+          headerTitleAlign: 'center'
+        }} />
+      </div>
+    </>    
   )
 }
